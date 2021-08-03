c8ff45b1c7ab07e580094ea72a10a1401496ec6e
christen-tautz <chrisneuss01@gmail.com>
2021-08-03 15:05:36 +0200

    fixed error on creating new variable

--- src/index.mjs
+++ src/index.mjs
@@ -101,7 +101,7 @@ async function dbPOST(name, res, req) {
             await db.collection("links").findOne({ name: name }).then(async docs => {
 
                 if (docs == undefined) {
-                    await db.collection("links").insertOne({ name: name, links: [] });
+                    docs = await db.collection("links").insertOne({ name: name, links: [] });
                 };
 
                 let upImage = req.files.image;
